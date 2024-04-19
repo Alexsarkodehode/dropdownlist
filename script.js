66a5efc2c8b4679c565e9dc3d2de6acfdcb60eb7
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2024-04-20 01:10:00 +0200

    js changes

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const dropDowns = document.querySelectorAll('.dropdown');
+let dropDowns = document.querySelector('.dropdown1');
+const dropDown1 = document.querySelector('.dropdown1');
+
+const dropDown2 = document.querySelector('.dropdown2');
 
 /* We write dropDown instead of dropDowns because the dropdowns are split into individual dropDowns */
 dropDowns.forEach(dropDown => {
-    dropDown.addEventListener('click', () => {
+    dropDowns.addEventListener('click', () => {
         /* querySelector is selecting the css class .active.
         javascript method remove() removes active from the classlist whom was added through html */
         document.querySelector('.active').classList.remove('active');
-        dropDown.classList.add('active');
+        dropDowns.classList.add('active');
     })
 })
+
+
