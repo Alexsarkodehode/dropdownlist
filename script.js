2415bf21a3e5dd8f9272f4a5ca259ae237afd383
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2024-04-22 09:24:20 +0200

    js changes: added forEach method.

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-let dropDowns = document.querySelector('.dropdown1');
-const dropDown1 = document.querySelector('.dropdown1');
+//we start with selecting the dropdown class and then we put it into a variable
+//After we select the dropdowns classes we chain the variable into a forEach method
+//Then we put an argument into the forEach method
+//
+/* we use the querySelectorAll to select multiple elements at the same time */
+const dropDowns = document.querySelectorAll('.dropdown');
 
-const dropDown2 = document.querySelector('.dropdown2');
+/**/
+dropDowns.forEach(dropdown => {
 
-/* We write dropDown instead of dropDowns because the dropdowns are split into individual dropDowns */
-/*  */
-dropDowns.forEach(dropDown => {
-    dropDowns.addEventListener('click', () => {
-        /* querySelector is selecting the css class .active.
-        javascript method remove() removes active from the classlist whom was added through html */
-        document.querySelector('.active').classList.remove('active');
-        dropDowns.classList.add('active');
-    })
 })
-
 
