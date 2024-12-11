7d047c269084264e2a9be057e9bbb2e3dd46d566
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2024-12-11 22:05:10 +0100

    ok

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
-let output = document.getElementById('output');
-      let dropdownList = document.getElementById("list");
-      dropdownList.style.display = "none";
-      function openDropdown() {
-         if (dropdownList.style.display != "none") {
-            dropdownList.style.display = "none";
-         } else {
-            dropdownList.style.display = "block";
-         }
-      }
-      const p_elements = document.getElementsByTagName("p");
-      // access all p elements
-      const totalP = p_elements.length;
-      // iterate through all <p> elements
-      for (let i = 0; i < totalP; i++) {
-         const option = p_elements[i];
-         // add event listner to <p> element
-         option.addEventListener("click", () => {
-            // When a user clicks on any p element, get its innerHTML
-            output.innerHTML = "The selected option is " + option.innerHTML;
-            // close the dropdown list once users select an option
-            dropdownList.style.display = "none";
-         })
-      }
+const myDiv = document.getElementsByClassName('backgroundDiv')
+
