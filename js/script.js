e36562db7db6906b9a9a20ed2e06774dfe7d4cdc
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2024-12-25 15:14:29 +0100

    setting background image

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ console.log(backgroundEl)
 backgroundEl = [title: 'backgroundImage' image: '../images/ancientjapanesehouse.jpg']
 scr.
 
+const backgroundImage = new Image(100, 200);
+backgroundImage.src = "picture.jpg";
+document.body.appendChild(myImage);
+
