17ab8860391f846f32cca551105b443b8be9511e
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2024-12-31 00:18:40 +0100

    l

--- js/script.js
+++ js/script.js
@@ -1,20 +1,7 @@
-const backgroundEl = document.getElementsByClassName(".backgroundDiv")
-console.log(backgroundEl)
 
-backgroundEl = [title: 'backgroundImage' image: '../images/ancientjapanesehouse.jpg']
-
-
-const backgroundImage = new Image(100, 200);
-backgroundImage.src = "ancientjapanesehouse.jpg";
-document.body.appendChild(myImage);
-
-document.innerHTML
-
-const myCar = 123
-console.log(my)
 /* 
 Create an HTML element to hold the image, such as an img tag.
 Set the src attribute of the img tag to the URL or file path of the image you want to display.
 Add the img tag to the HTML document using the document. */
 
-const myImage = document.i
+const myImage = document.getElementById("#")
