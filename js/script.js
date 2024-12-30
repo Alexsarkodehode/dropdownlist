a646bed35ef02b5873814308e7dfc8d050cdb212
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2024-12-30 23:49:57 +0100

    ok

--- js/script.js
+++ js/script.js
@@ -16,3 +16,5 @@ console.log(my)
 Create an HTML element to hold the image, such as an img tag.
 Set the src attribute of the img tag to the URL or file path of the image you want to display.
 Add the img tag to the HTML document using the document. */
+
+const myImage = document.i
