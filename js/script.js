d84f18e08ff705924e6871567a30a6158f7322a7
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2025-01-02 23:48:04 +0100

    l

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-const myImage = document.querySelector("#myImage")
-console.log(myImage)
 
-const img_1 = document.querySelector("img")
+
+/* const img_1 = document.querySelector("img")
 console.log(img_1)
-img_1.src = "../images/ancientjapanesehouse.jpg"
+img_1.src = "../images/ancientjapanesehouse.jpg" */
+
+
 
