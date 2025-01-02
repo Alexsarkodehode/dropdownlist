ae027c665ec6b1d25cf732ecc73121393e213457
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2025-01-02 23:52:48 +0100

    allright

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@
 /* const img_1 = document.querySelector("img")
 console.log(img_1)
 img_1.src = "../images/ancientjapanesehouse.jpg" */
+/* 
+const img = document.createElement("img")
+img.src = "../images/ancientjapanesehouse.jpg"
+document.body.appendChild(img) */
 
 
 
