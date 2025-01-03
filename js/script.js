771faa052503eaa78920c92e54b60c62259c71c9
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2025-01-04 00:39:45 +0100

    ok

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-
+//method one
 
 /* const img_1 = document.querySelector("img")
 console.log(img_1)
 img_1.src = "../images/ancientjapanesehouse.jpg" */
 
+//method two 
+
 const img = document.createElement("img")
 img.src = "../images/ancientjapanesehouse.jpg"
-
 
 img.style.width = "100%"
 img.style.height = "100%"
