bf49a80be9ed899b30bf713d9a531bff49f4047a
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2025-01-04 01:36:09 +0100

    .

--- js/script.js
+++ js/script.js
@@ -17,8 +17,13 @@ document.body.appendChild(img) */
 
 //method three 
 
-const my_images = document.querySelectorAll(img)
+const my_images = document.querySelectorAll("img")
+
 console.log(my_images)
+
+my_images[0].src = "../images/ancientjapanesehouse.jpg"
+my_images[1].src = "../images/architect-9280053_1280.jpg"
+my_images[2].src = "../images/beach1.jpg.jpg"
 
 
 
